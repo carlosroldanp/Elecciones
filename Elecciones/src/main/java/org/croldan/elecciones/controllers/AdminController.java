f548e4926bf0996171296d3cab4f0269249e7513
Carlos <carlosroldanp@gmail.com>
2021-05-13 10:59:58 +0200

    Reestructuracion del admin

--- Elecciones/src/main/java/org/croldan/elecciones/controllers/AdminController.java
+++ Elecciones/src/main/java/org/croldan/elecciones/controllers/AdminController.java
@@ -3,37 +3,58 @@ package org.croldan.elecciones.controllers;
 import java.time.LocalDate;
 
 import org.croldan.elecciones.entities.Eleccion;
+import org.croldan.elecciones.repositories.CandidaturaRepository;
 import org.croldan.elecciones.repositories.EleccionRepository;
+import org.croldan.elecciones.repositories.PartidoPoliticoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
 	@Autowired
 	private EleccionRepository eleccionRepository;
 
+	@Autowired
+	private PartidoPoliticoRepository partidoPoliticoRepository;
+
+	@Autowired
+	private CandidaturaRepository candidaturaRepository;
+
 	@GetMapping("/eleccion/c")
-	public String cGet(ModelMap m) {
+	public String cEleccionGet(ModelMap m) {
 		m.put("view", "admin/eleccion/c");
 		return "/_t/frame";
 	}
 
 	@PostMapping("/eleccion/c")
-	public String cPost(@RequestParam("fecha") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
+	public String cEleccionPost(@RequestParam("fecha") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha) {
 		eleccionRepository.save(new Eleccion(fecha));
-		return "redirect:/eleccion/r";
+		return "redirect:admin/eleccion/r";
 	}
 
 	@GetMapping("/eleccion/r")
-	public String rGet(ModelMap m) {
+	public String rEleccionGet(ModelMap m) {
 		m.put("elecciones", eleccionRepository.findAll());
 		m.put("view", "anon/eleccion/r");
 		return "/_t/frame";
+	}
+
+	// ============================
+	// CANDIDATURA
+	// ============================
+
+	@GetMapping("candidatura/c")
+	public String cCandidaturaGet(ModelMap m) {
+		
+		m.put("elecciones", eleccionRepository.findAll());
+		return null;
 	}
 }
